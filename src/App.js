d336f9baa92ccae997f8a455e2f0ab8e1ada76f9
Deepak Motlani <motlani.deepak@gmail.com>
2018-05-20 14:46:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Index from './views/index';
+
+class App extends Component {
+  render() {	  
+    return (
+      <div className="App">
+		<Index/>
+      </div>
+    );
+  }
+}
+
+export default App;
